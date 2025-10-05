a5297fa85c9d08454e21159c7a64365dfe8bbe02
rajuyadav03 <162599391+rajuyadav03@users.noreply.github.com>
2025-10-06 01:23:25 +0530

    subarray_productupdate 152

--- subarray_product.cpp
+++ subarray_product.cpp
@@ -1,32 +1,36 @@
 #include <iostream>
-#include <vector>   
+#include <vector>
 #include <climits>
+#include <algorithm>
 using namespace std;
- class solution{
-    public:
-    int subarrayproduct(vector<int>& nums){
+
+class Solution {
+public:
+    int subarrayProduct(vector<int>& nums) {
         int minproduct = nums[0];
         int maxproduct = nums[0];
-        int result = maxproduct;
-    
-        for (int i = 1; i< nums.size();i++){
+        int result = nums[0];
+
+        for (int i = 1; i < nums.size(); i++) {
             int currentproduct = nums[i];
-            int temp = max(currentproduct,max(maxproduct*currentproduct,minproduct*currentproduct));
-            minproduct = min(currentproduct,min(minproduct*currentproduct,maxproduct*currentproduct));
-            maxproduct = temp;
-            result = max(result,maxproduct);
-           
-    
-        } 
-    
+
+            int tempMax = maxproduct;
+            int tempMin = minproduct;
+
+            maxproduct = max({currentproduct, tempMax * currentproduct, tempMin * currentproduct});
+            minproduct = min({currentproduct, tempMax * currentproduct, tempMin * currentproduct});
+
+            result = max(result, maxproduct);
+        }
+
         return result;
     }
+};
 
- };
- int main(){
-    vector<int> nums = {2,3,-2,4};
-    solution obj;
-    int result = obj.subarrayproduct(nums);
+int main() {
+    vector<int> nums = {2, 3, -2, 4};
+    Solution obj;
+    int result = obj.subarrayProduct(nums);
     cout << "Maximum subarray product is: " << result << endl;
     return 0;
- }
+}
